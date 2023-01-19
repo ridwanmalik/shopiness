b65fb674f438fe10a1a10487560a9ea7b786b76d
ridwanmalik <skredonemalik@gmail.com>
2023-01-20 03:08:56 +0600

    Removing Extras

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
-import './globals.css'
+import { ReactNode } from "react"
+import "./globals.css"
+import Head from "./head"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <Head />
       <body>{children}</body>
     </html>
   )
